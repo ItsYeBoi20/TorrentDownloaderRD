55983c33f7fce3b29fcfefbff797f6477817f5c7
Neo Fercho <fercho.neo@gmail.com>
2024-08-29 15:33:41 +0200

    Version File
    Only for the people who downloaded the application before I renamed the folder structure. Now "Check for Update" should work for them until they decide to update.

---
+++ MediaDownloader/Settings.cs
@@ -0,0 +1,22 @@
+﻿// This file is only for the people who downloaded the previous version before I renamed the files
+
+using RealDebridAPI;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaDownloader
+{
+    public partial class Settings : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+        private const int IDC_HAND = 32649;
+
+        public static string currentVersion = "1.0.2";
+    }
+}
